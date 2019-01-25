abba872a17ebdb803db925fb09edc3469e8c28f7
max <maxwfarley@live.co.uk>
2019-01-25 21:34:05 +0000

    Started on the UI that will control the penguins and which resources they go and collect. Added the Actions to the UI

---
+++ Penguin_March/Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public int startPenguinNum = 25;
+    [HideInInspector]
+    public int currPenguinNum;
+
+    GameObject HUD_GO;
+    HUD_Manager HUD_M;
+
+    int penguinCounter;
+    string action;
+
+	// Use this for initialization
+	void Start ()
+    {
+        // Spawn Penguins.
+        currPenguinNum = startPenguinNum;
+
+        GameObject penguinPrefab = (GameObject)Resources.Load("Penguin");
+        for(int i = 0; i < startPenguinNum; i++)
+        {
+            Instantiate(penguinPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        }
+
+        // HUD manager
+        HUD_M = new HUD_Manager();
+        HUD_GO = GameObject.Find("HUD");
+        //HUD_GO = Instantiate(HUD_GO, Vector3.zero, Quaternion.identity);
+        HUD_M.Start(HUD_GO);
+	}
+
+    public void DoAction(string action)
+    {
+        HUD_M.EnableNumChooser();
+        this.action = action;
+    }
+
+    public void CountPenguins(int inc)
+    {
+        penguinCounter += inc;
+
+        if (penguinCounter < 0) { penguinCounter = 0; }
+        else if (penguinCounter > currPenguinNum) { penguinCounter = currPenguinNum; }
+        HUD_M.UpdateCounterHUD(penguinCounter);
+    }
+
+    public void ConfirmPenguinNum()
+    {
+        Debug.Log(action + ", penguins:" + penguinCounter);
+        HUD_M.EnableBase();
+    }
+}
---
+++ Penguin_March/Assets/Scripts/HUD_Manager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD_Manager {
+
+    GameObject HUD;
+
+    GameObject baseMenu;
+    GameObject NumberChooser;
+
+    Text penguinAssignNumberText;
+
+    public void Start(GameObject HUD)
+    {
+        this.HUD = HUD;
+
+
+        baseMenu = HUD.transform.Find("BaseMenu").gameObject;
+        NumberChooser = HUD.transform.Find("NumberChooser").gameObject;
+        penguinAssignNumberText = GameObject.Find("Counter").GetComponentInChildren<Text>();
+
+        NumberChooser.SetActive(false);
+
+
+    }
+
+    public void EnableNumChooser()
+    {
+        HideAll();
+        NumberChooser.SetActive(true);
+    }
+
+    void HideAll()
+    {
+        baseMenu.SetActive(false);
+        NumberChooser.SetActive(false);
+    }
+
+    public void UpdateCounterHUD(int numToDisplay)
+    {
+        penguinAssignNumberText.text = "" + numToDisplay;
+    }
+
+    public void EnableBase()
+    {
+        HideAll();
+        baseMenu.SetActive(true);
+    }
+	
+}
